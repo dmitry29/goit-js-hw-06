bffdf217e90e8ce008d124adeffa10082aef85eb
dmitry29 <dmitry29push@gmail.com>
2022-09-01 15:32:37 +0300

    1.09.2022

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,14 @@
+/**
+ * !Задание № 10
+ * Создай функцию createBoxes(amount), которая принимает один параметр - число. 
+ * Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
+
+Размеры самого первого <div> - 30px на 30px.
+Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
+Все элементы должены иметь случайный цвет фона в формате HEX. 
+Используй готовую функцию getRandomHexColor для получения цвета.
+ */
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
@@ -9,46 +20,36 @@ const refs = {
   boxesCountainerEl: document.querySelector("#boxes"),
 };
 
-let baseSizes = 20;
-let addValue = 10;
+refs.createBtnEl.addEventListener("click", createBoxes);
+refs.destroyBtnEl.addEventListener("click", onDestroyBtnElClick);
 
-refs.inputEl.addEventListener("input", onInputClick);
+//! Цикл создания div
 
-//! Получаем значение amount
-function onInputClick(event) {
-  let amount = event.currentTarget.value;
+function createBoxes(amount) {
+  amount = refs.inputEl.value;
   console.log("Количество дивов -", amount);
-
+  // let baseSizes = 20;
+  // let addValue = 10;
   const elements = [];
 
-  refs.createBtnEl.addEventListener("click", createBoxes);
-  refs.destroyBtnEl.addEventListener("click", onDestroyBtnElClick);
-
-  //! Цикл создания div
-  function createBoxes() {
-    for (let i = 1; i <= amount; i += 1) {
-      console.log("amount", amount);
-
-      const divEl = document.createElement("div");
-      divEl.style.backgroundColor = getRandomHexColor();
-      divEl.style.width = baseSizes + addValue + `px`;
-      divEl.style.height = baseSizes + addValue + `px`;
-      addValue += 10;
-      console.log("width", divEl.style.width);
-      console.log("height", divEl.style.height);
-      elements.push(divEl);
-    }
-    refs.boxesCountainerEl.append(...elements);
+  for (let i = 1; i <= amount; i += 1) {
+    const divEl = document.createElement("div");
+    divEl.style.backgroundColor = getRandomHexColor();
+    // divEl.style.width = baseSizes + addValue + `px`;
+    // divEl.style.height = baseSizes + addValue + `px`;
+    divEl.style.width = `${30 + i * 10}px`;
+    divEl.style.height = `${30 + i * 10}px`;
+    // addValue += 10;
+    console.log("width", divEl.style.width);
+    console.log("height", divEl.style.height);
+    elements.push(divEl);
   }
+  refs.boxesCountainerEl.append(...elements);
+}
 
-  //! Очищаем div#boxes
+//! Очищаем div#boxes
 function onDestroyBtnElClick() {
   refs.boxesCountainerEl.innerHTML = "";
   refs.inputEl.value = "";
-  baseSizes = 20;
-
   console.log("div#boxes очищен!");
 }
-
-
-} 
---
+++ src/js/task-12.js
@@ -0,0 +1,12 @@
+const list = document.querySelector(".list");
+
+const newTechnologies = ["React", "TypeScript", "Node.js"];
+const markup = newTechnologies
+  .map((technology) => `<li class="list-item new">${technology}</li>`)
+  .join("");
+  
+
+list.insertAdjacentHTML("beforeend", markup);
+list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>");
+
+const markup1 = newLink.map((link) => `<a class="link-item">${click}</a>`).join("");
